a1334df544f5977c9a5ca97c5e01b5431cd70229
Jayanta <bjayanta.me@gmail.com>
2023-01-15 01:05:31 +0600

    update page.php

--- page.php
+++ page.php
@@ -7,7 +7,7 @@
 
 <!-- Page banner -->
 <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg'); ?>)"></div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
