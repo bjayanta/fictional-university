c8cce8ec1521ced925a1eb35ab9abc354458054f
Jayanta <bjayanta.me@gmail.com>
2023-01-30 19:09:13 +0600

    update custom post

--- front-page.php
+++ front-page.php
@@ -13,38 +13,42 @@
 
 <!-- group -->
 <div class="full-width-split group">
+    <!-- Events -->
     <div class="full-width-split__one">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
+            <?php
+            $homePageEvents = new WP_Query([
+                'posts_per_page' => 2,
+                'post_type' => 'event'
+            ]);
+
+            while ($homePageEvents->have_posts()) {
+                $homePageEvents->the_post();
+            ?>
+
             <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Mar</span>
-                    <span class="event-summary__day">25</span>
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month"><?php the_time('M'); ?></span>
+                    <span class="event-summary__day"><?php the_time('d'); ?></span>
                 </a>
 
                 <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Poetry in the 100</a></h5>
-                    <p>Bring poems you&rsquo;ve wrote to the 100 building this Tuesday for an open mic and snacks. <a href="#" class="nu gray">Learn more</a></p>
+                    <h5 class="event-summary__title headline headline--tiny">
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h5>
+                    <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="#" class="nu gray">Learn more</a></p>
                 </div>
             </div>
 
-            <div class="event-summary">
-                <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month">Apr</span>
-                    <span class="event-summary__day">02</span>
-                </a>
-                
-                <div class="event-summary__content">
-                    <h5 class="event-summary__title headline headline--tiny"><a href="#">Quad Picnic Party</a></h5>
-                    <p>Live music, a taco truck and more can found in our third annual quad picnic day. <a href="#" class="nu gray">Learn more</a></p>
-                </div>
-            </div>
+            <?php } wp_reset_postdata(); ?>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin"><a href="<?php the_permalink(); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
     </div>
 
+    <!-- Recent posts -->
     <div class="full-width-split__two">
         <div class="full-width-split__inner">
             <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
--- functions.php
+++ functions.php
@@ -31,14 +31,15 @@ function university_features() {
 
 add_action('after_setup_theme', 'university_features');
 
-function university_post_types() {
-    register_post_type('event', [
-        'public' => true,
-        'menu_icon' => 'dashicons-calendar',
-        'labels' => [
-            'name' => 'Events'
-        ],
-    ]);
-}
+// Custom Post:
+// function university_post_types() {
+//     register_post_type('event', [
+//         'public' => true,
+//         'menu_icon' => 'dashicons-calendar',
+//         'labels' => [
+//             'name' => 'Events'
+//         ],
+//     ]);
+// }
 
-add_action('init', 'university_post_types');
+// add_action('init', 'university_post_types');
